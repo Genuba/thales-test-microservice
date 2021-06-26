c9146ddf73984d809e5f54658719c375bb332256
genuba <daalpiva@gmail.com>
2021-06-26 11:11:03 -0500

    first commit

---
+++ src/test/java/com/example/thalestestmicroservice/ThalesTestMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thalestestmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThalesTestMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
